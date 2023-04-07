80d875c4eaba459cd69c9bf7a57f27af93ac9e56
ebalu0776 <ebalu0776@gmail.com>
2023-04-07 09:07:47 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+    <div>
+        <h1 className = heading>Congratulations</h1>
+        <p>Vishnu Institute Of Computer Education </p>
+        <img src = "https://assets.ccbp.in/frontend/react-js/congrats-card-bg.png" />
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
